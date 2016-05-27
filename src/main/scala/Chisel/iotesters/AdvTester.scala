38de0e50bcf748ac5ac2dd9692ff482a736d45bd
Danny <dt27182@gmail.com>
2016-05-27 15:31:09 -0700

    change AdvTests declaration to match new PeekPokeTests

--- src/main/scala/Chisel/iotesters/AdvTester.scala
+++ src/main/scala/Chisel/iotesters/AdvTester.scala
@@ -7,7 +7,7 @@ import Chisel._
 import scala.collection.mutable.ArrayBuffer
 
 // Provides a template to define advanced tester transactions
-trait AdvTests extends ClassicTests {
+trait AdvTests extends PeekPokeTests {
   def cycles: Long
   def wire_poke(port: Bits, target: BigInt):  Unit
   def reg_poke(port: Bits, target: BigInt):   Unit
