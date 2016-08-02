5c25eecfa5f162f568f6ac529b60b26b01d9f435
chick <chick@qrhino.com>
2016-08-02 14:45:16 -0700

    Create a peek poke test, there should be more of these

--- src/test/scala/examples/GCDUnitTest.scala
+++ src/test/scala/examples/GCDUnitTest.scala
@@ -4,7 +4,8 @@ package examples
 
 import chisel3._
 import chisel3.util._
-import chisel3.iotesters.{SteppedHWIOTester, ChiselFlatSpec}
+import chisel3.iotesters._
+import org.scalatest.{Matchers, FlatSpec}
 
 class GCD extends Module {
   val int_width = 16
@@ -64,11 +65,43 @@ class GCDTester extends ChiselFlatSpec {
   }
 }
 
-//object GCDUnitTest {
-//  def main(args: Array[String]): Unit = {
-//    val tutorial_args = args.slice(1, args.length)
-//
-//    new GCDTester
-//  }
-//}
-//
+class GCDPeekPokeTester(gcd: GCD, backend: Option[Backend] = None) extends PeekPokeTester(gcd, _backend = backend) {
+  def computeGcd(a: Int, b: Int): (Int, Int) = {
+    var x = a
+    var y = b
+    var depth = 1
+    while(y > 0 ) {
+      if (x > y) {
+        x -= y
+      }
+      else {
+        y -= x
+      }
+      depth += 1
+    }
+    (x, depth)
+  }
+
+  val (a, b, z) = (64, 48, 16)
+
+  poke(gcd.io.a, a)
+  poke(gcd.io.b, b)
+  poke(gcd.io.e, 1)
+  step(1)
+  poke(gcd.io.e, 0)
+
+  val (expected_gcd, steps) = computeGcd(a, b)
+
+  step(steps - 1) // -1 is because we step(1) already to toggle the enable
+  expect(gcd.io.z, expected_gcd)
+  expect(gcd.io.v, 1)
+}
+
+class GCDSpec extends FlatSpec with Matchers {
+  "gcd" should "run ok with peekpoketester too" in {
+    runPeekPokeTester(() => new GCD, "firrtl"){
+      (c,b) => new GCDPeekPokeTester(c,b)} should be (true)
+  }
+
+}
+
