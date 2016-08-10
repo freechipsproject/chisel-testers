2f76e3a16394a5fada4bb459d3c4dfbae7a48767
chick <chick@qrhino.com>
2016-08-10 13:04:54 -0700

    Change firrtl file suffix to be ".ir" Added GCDSpec which does GCD but uses peek poke tester Changed router to not be ignored, it works for me Fixed style errors in string interpolation

--- src/test/scala/examples/Router.scala
+++ src/test/scala/examples/Router.scala
@@ -150,7 +150,7 @@ class RouterUnitTester(number_of_packets_to_send: Int) extends OrderedDecoupledH
 
 class RouterUnitTesterSpec extends ChiselFlatSpec {
   val number_of_packets = 20
-  "a router" should "can have it's rout table loaded and changed and route a bunch of packets" ignore {
+  "a router" should "can have it's rout table loaded and changed and route a bunch of packets" in {
     assertTesterPasses {
       new RouterUnitTester(number_of_packets)
     }
