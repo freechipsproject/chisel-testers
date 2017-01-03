63ec6628bfdbe9656e7722e5eddd76e2785a623f
Chick Markley <chick@qrhino.com>
2017-01-03 11:50:31 -0800

    Poking large longs was converting longs to int and losing large values. (#76)
    Fixed by making long specific poke functions
    Added test for behavior reported in Issue #425

---
+++ src/test/scala/examples/BigNumbers.scala
@@ -0,0 +1,39 @@
+// See LICENSE for license details.
+
+package examples
+
+import chisel3._
+import chisel3.iotesters.{PeekPokeTester, TesterOptionsManager}
+import org.scalatest.{FreeSpec, Matchers}
+
+class PassOn extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(64.W))
+    val out = Output(UInt(64.W))
+  })
+  io.out := io.in
+}
+
+class BigNumbersTester(c: PassOn) extends PeekPokeTester(c) {
+  poke (c.io.in, 0x0000000070000000L)
+  expect(c.io.out, 0x0000000070000000L)
+
+  // Test 2:(Test Fails)
+  poke (c.io.in, 0x0000000770000000L)
+  expect (c.io.out, 0x0000000770000000L)
+
+  // Output only takes value of last 32 bits (70000000) and test fails.
+
+  // Test 3:(FIRRTL generates an error)
+  poke (c.io.in, 0x0000000080000000L)
+  expect (c.io.out, 0x0000000080000000L)
+}
+
+class BigNumbersSpec extends FreeSpec with Matchers {
+  "big numbers should work with interpreter backend" in {
+    iotesters.Driver.execute(() => new PassOn, new TesterOptionsManager) { c =>
+      new BigNumbersTester(c)
+    } should be(true)
+
+  }
+}
