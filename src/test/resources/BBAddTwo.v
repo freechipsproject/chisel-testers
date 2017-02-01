98d4b9e53e39bbc83b92aac51b40d03fa83af15d
Jim Lawson <ucbjrl@berkeley.edu>
2017-02-01 14:43:50 -0800

    Fix dropped edit (single module in each resource); add ending newline.

--- src/test/resources/BBAddTwo.v
+++ src/test/resources/BBAddTwo.v
@@ -6,12 +6,3 @@ module BBAddTwo(
   out <= in + 2;
   end
 endmodule
-
-module BBAddThree(
-    input  [15:0] in,
-    output reg [15:0] out
-);
-  always @* begin
-  out <= in + 3;
-  end
-endmodule
