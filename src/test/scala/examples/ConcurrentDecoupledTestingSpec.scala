892fa655de71c72949177617877ab51036007d2b
chick <chick@qrhino.com>
2017-03-06 11:16:16 -0800

    Made MultiGcdCalculator parameterizable on number of engines.

--- src/test/scala/examples/ConcurrentDecoupledTestingSpec.scala
+++ src/test/scala/examples/ConcurrentDecoupledTestingSpec.scala
@@ -5,7 +5,6 @@ package examples
 import chisel3._
 import chisel3.util._
 import chisel3.iotesters._
-import firrtl_interpreter.InterpreterOptions
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.collection.mutable
@@ -26,14 +25,13 @@ case class GcdResult(x: Int, y: Int, gcd: Int) {
 }
 
 /**
-  * This module can calculate two independent gcd results in parallel
+  * This module can calculate n independent gcd results in parallel
+  * @param numberOfEngines the number of parallel gcd engines in circuit
   */
-class DualGcdCalculator extends Module {
+class MultiGcdCalculator(val numberOfEngines: Int) extends Module {
   val io  = IO(new Bundle {
-    val in1  = Flipped(Decoupled(new GcdInput()))
-    val out1 = Valid(new GcdOutput)
-    val in2  = Flipped(Decoupled(new GcdInput()))
-    val out2 = Valid(new GcdOutput)
+    val input  = Flipped(Vec(numberOfEngines, Decoupled(new GcdInput())))
+    val output = Vec(numberOfEngines, Valid(new GcdOutput))
   })
 
   /**
@@ -80,44 +78,43 @@ class DualGcdCalculator extends Module {
     }
   }
 
-  gcdLogic(io.in1, io.out1)
-  gcdLogic(io.in2, io.out2)
+  for(i <- 0 until numberOfEngines) {
+    gcdLogic(io.input(i), io.output(i))
+  }
 }
 
-class DualGcdCalculatorTester(c: DualGcdCalculator) extends AdvTester(c)  {
-  val gcdOutputHandler1 = new ValidSink(c.io.out1, (outPort: GcdOutput) => {
-    GcdResult(peek(outPort.a).toInt, peek(outPort.b).toInt, peek(outPort.gcd).toInt)
-  })
-  val gcdInputDriver1 = new DecoupledSource(c.io.in1, (inPorts: GcdInput, inValues: TestGCD3Values) => {
-    wire_poke(inPorts.a, inValues.a)
-    wire_poke(inPorts.b, inValues.b)
-  })
-  val gcdOutputHandler2 = new ValidSink(c.io.out2, (outPort: GcdOutput) => {
-    GcdResult(peek(outPort.a).toInt, peek(outPort.b).toInt, peek(outPort.gcd).toInt)
-  })
-  val gcdInputDriver2 = new DecoupledSource(c.io.in2, (inPorts: GcdInput, inValues: TestGCD3Values) => {
-    wire_poke(inPorts.a, inValues.a)
-    wire_poke(inPorts.b, inValues.b)
-  })
+class MultiGcdCalculatorTester(c: MultiGcdCalculator) extends AdvTester(c)  {
+  private val numberOfSamples = ConcurrentDecoupledTestingSpec.numberOfSamples
+
+  private val outputHandlers = c.io.output.map { out =>
+    new ValidSink(out, (outPort: GcdOutput) => {
+      GcdResult(peek(outPort.a).toInt, peek(outPort.b).toInt, peek(outPort.gcd).toInt)
+    })
+  }
+  private val inputHandlers = c.io.input.map { in =>
+    new DecoupledSource(in, (inPorts: GcdInput, inValues: TestGCD3Values) => {
+      wire_poke(inPorts.a, inValues.a)
+      wire_poke(inPorts.b, inValues.b)
+    })
+  }
 
   var resultCount = 0
   var valuesInputCount: Int = 0
 
-  def addInput(name: String, driver: DecoupledSource[GcdInput, TestGCD3Values]): Unit = {
+  def addInput(driverNumber: Int, driver: DecoupledSource[GcdInput, TestGCD3Values]): Unit = {
     val inputData = pairsToProcess.dequeue()
     driver.inputs.enqueue(inputData)
     valuesInputCount += 1
-    // println(s"Input: $name $inputData count $valuesInputCount")
+     println(s"Input: $driverNumber $inputData count $valuesInputCount")
   }
 
-  private val pairsToProcess = mutable.Queue.fill(100) { TestGCD3Values(rnd.nextInt(100) + 1, rnd.nextInt(100) + 1)}
+  private val pairsToProcess = mutable.Queue.fill(numberOfSamples) {
+    TestGCD3Values(rnd.nextInt(numberOfSamples) + 1, rnd.nextInt(numberOfSamples) + 1)
+  }
 
-  // prime the engine
-  addInput("1", gcdInputDriver1)
-  addInput("2", gcdInputDriver2)
-
-  while(pairsToProcess.nonEmpty) {
-    checkOutput()
+  // give everybody something to do
+  for((driver, index) <- inputHandlers.zipWithIndex) {
+    addInput(index, driver)
   }
 
   def checkOutput(): Unit = {
@@ -125,35 +122,37 @@ class DualGcdCalculatorTester(c: DualGcdCalculator) extends AdvTester(c)  {
       val (e, _) = GCDCalculator.computeGcdResultsAndCycles(result.x, result.y)
       if(e == result.gcd) "ok" else s"FAILED, got ${result.gcd} should be $e"
     }
-    if(gcdOutputHandler1.outputs.nonEmpty) {
-      resultCount += 1
-      val result = gcdOutputHandler1.outputs.dequeue()
-      println(s"handler1 event $resultCount got output $result ${ok(result)}")
-      if(pairsToProcess.nonEmpty) {
-        addInput("1", gcdInputDriver1)
-      }
-    }
-    if(gcdOutputHandler2.outputs.nonEmpty) {
-      resultCount += 1
-      val result = gcdOutputHandler2.outputs.dequeue()
-      println(s"handler2 event $resultCount got output $result ${ok(result)}")
-      if(pairsToProcess.nonEmpty) {
-        addInput("2", gcdInputDriver2)
+    outputHandlers.zipWithIndex.foreach { case (handler, index) =>
+      if(handler.outputs.nonEmpty) {
+        resultCount += 1
+        val result = handler.outputs.dequeue()
+        println(f"handler $index%3d event $resultCount%5d got output $result ${ok(result)}")
+        if (pairsToProcess.nonEmpty) {
+          addInput(index, inputHandlers(index))
+        }
       }
     }
     takestep()
   }
 
   //noinspection LoopVariableNotUpdated
-  while(resultCount < 100) {
+  while(resultCount < numberOfSamples) {
     checkOutput()
   }
 }
 
+object ConcurrentDecoupledTestingSpec {
+  val parallelEngines = 4
+  val numberOfSamples = 100
+}
+
 class ConcurrentDecoupledTestingSpec extends FreeSpec with Matchers {
   "This demonstrates waiting on two independent decoupled interfaces" in {
-    chisel3.iotesters.Driver(() => new DualGcdCalculator) { c =>
-      new DualGcdCalculatorTester(c)
+    chisel3.iotesters.Driver.execute(
+      Array("--backend-name", "firrtl"),
+      () => new MultiGcdCalculator(ConcurrentDecoupledTestingSpec.parallelEngines)
+    ) { c =>
+      new MultiGcdCalculatorTester(c)
      } should be(true)
   }
 }
