6dc79145af2902ff2b43fd517bedff3dca0f1ce1
Chick Markley <chick@qrhino.com>
2017-03-13 09:49:36 -0700

    Fix compiler errors due to firrtl emitter api changes (#125)
    * Reworked the VerilatorCppHarnessCompiler stuff.  It seemed to be unnecessarily using Transforms to do a simple template substitution.
    chiselMain had it's own hooks into the above.  Cleaned this up as much as I dared
    Fix deprecated code
    Reg(init -> RegInit(
    log2Up -> Log2Ceil
    Fixed up style (I needed this to debug this mess)
    
    * Got rid of chisel2 Module references
    
    * Incorporate Angie's Bundle -> Record changes for PeekPokeTesterUtils

--- src/test/scala/verilator/doohickey.scala
+++ src/test/scala/verilator/doohickey.scala
@@ -2,10 +2,13 @@ package verilator
 
 import chisel3._
 
+/**
+  * The practice of creating Seq's or Vec's of IO's is problematic
+  */
 class doohickey() extends Module {
-  val io = new Bundle {
-  }
-  val bobs = Vec.fill(16) {
+  val io = IO(new Bundle {})
+  //TODO: figure out if this idiom is the key point here or if some other example would suffice
+  val bobs = Seq.fill(16) {
     Module(new thingamabob()).io
   }
 }
