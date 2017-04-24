95bdb77d69b046f7a48d0d2ada462b6a8adc44b7
Jim Lawson <ucbjrl@berkeley.edu>
2017-04-24 15:28:49 -0700

    Use createTestDirectory to ensure a pristine enviroment - fixes #132.

--- src/test/scala/verilator/Verilator.scala
+++ src/test/scala/verilator/Verilator.scala
@@ -1,19 +1,24 @@
 package verilator
 
 
-import chisel3._
 import chisel3.iotesters.chiselMain
+import chisel3.Driver.createTestDirectory
 import org.scalatest._
-import chisel3._
 
 class VerilatorTest extends FlatSpec with Matchers {
+  // Ensure we run this test in a clean directory to avoid stale files such as black_box_verilog_files.f
+  //  See issue #132 - https://github.com/ucb-bar/chisel-testers/issues/132
+  //  and issue #504 - https://github.com/ucb-bar/firrtl/issues/504
+  val targetDir = createTestDirectory("ChiselMainVerilatorTest")
   "The Verilator backend" should "be able to compile the cpp code" in {
       val args = Array[String]("--v",
           "--backend",
           "verilator",
           "--compile",
           "--genHarness",
-          "--minimumCompatibility", "3.0.0")
+          "--minimumCompatibility", "3.0.0",
+          "--targetDir", targetDir.getPath
+      )
       chiselMain(args, () => new doohickey())
     }
 }
