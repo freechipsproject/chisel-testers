3466008c8e036d2534c1a2c85f3601cfc7464824
chick <chick@qrhino.com>
2017-06-07 10:39:37 -0700

    Add a bunch of comments

--- src/test/scala/examples/AccumBlackBoxSpec.scala
+++ src/test/scala/examples/AccumBlackBoxSpec.scala
@@ -10,6 +10,18 @@ import chisel3.iotesters._
 
 import chisel3.experimental._
 
+/**
+  * The Accum tests below illustrate the following.
+  * Black boxes generally contain some verilog code that is linked in to the generated circuit.
+  * When running simulations with the firrtl interpreter, verilog black boxes cannot currently be run.
+  * As a work around the interpreter allows the developer to construct a scala implementation of the black box.
+  * In this example it is a scala implementation of a black box that maintains some
+  * kind of state withing the black box.  In verilog this is done in registers, but in a scala implementation it
+  * must be one some other way.  Here we maintain state in a some class variables. A scala black box implementation
+  * is only active when it's execute or cycle method is called.  Because cycle does not provide the state of the inputs
+  * if cycle needs to access them then the execute call should put those inputs in an accessible place.
+  */
+
 trait AccumAbstractIfc extends BaseModule {
   val io = IO{
     new Bundle{
@@ -24,6 +36,11 @@ class AccumIfc extends Module with AccumAbstractIfc
 import firrtl.ir.Type
 import firrtl_interpreter._
 
+/**
+  * This is an implementation of a black box whose verilog is contained inline in AccumBlackBox, an instance of this
+  * class will be placed into a black box factory so that it can be passed properly to the firrtl interpreter
+  * @param name black box name
+  */
 class AccumFirrtlInterpreterBlackBox( val name : String) extends BlackBoxImplementation {
 
   var ns : BigInt = 0
@@ -46,6 +63,9 @@ class AccumFirrtlInterpreterBlackBox( val name : String) extends BlackBoxImpleme
   }
 }
 
+/**
+  * The factor that will provide firrtl access to the implementations
+  */
 class AccumBlackBoxFactory extends BlackBoxFactory {
 
   def createInstance(instanceName: String, blackBoxName: String): Option[BlackBoxImplementation] = {
@@ -76,6 +96,11 @@ class AccumBlackBoxWrapper extends AccumIfc {
   printf("m.io.data %d io.data %d\n", m.io.data, io.data)
 }
 
+/**
+  * The value of the accumulator in the black box implementation may have advanced several times
+  * because of simulation warm-up.  We find the initial value and see that it advances as expected
+  * @param c the dut
+  */
 class Accum_PeekPokeTester[T <: AccumIfc](c:T) extends PeekPokeTester(c) {
   val initialValue: BigInt = peek( c.io.data)
   step(1)
