657a0c03cf338e7c57bf65707bdfc04258fe0825
chick <chickmarkley@gmail.com>
2017-09-18 14:50:38 -0700

    Add more to tests to show that data written with pokeMemory can be read through conventional memory access

--- src/test/scala/chisel3/iotesters/MemPokeSpec.scala
+++ src/test/scala/chisel3/iotesters/MemPokeSpec.scala
@@ -3,18 +3,26 @@
 package chisel3.iotesters
 
 import chisel3._
+import chisel3.util.log2Ceil
 import org.scalacheck.Prop._
 import org.scalatest.prop.Checkers
 
 class InnerMemModule extends Module {
-  val io: Bundle = IO(new Bundle)
+  //noinspection TypeAnnotation
+  val io = IO(new Bundle)
   val nelly = Mem(1024, UInt(32.W))
 }
 
 class OuterMemModule extends Module {
-  val io: Bundle = IO(new Bundle)
+  //noinspection TypeAnnotation
+  val io = IO(new Bundle {
+    val readAddress = Input(UInt(log2Ceil(1024).W))
+    val readData    = Output(UInt(32.W))
+  })
   val billy = Mem(1024, UInt(32.W))
   val inner = Module(new InnerMemModule)
+
+  io.readData := billy(io.readAddress)
 }
 
 class MemPokeTester(m: OuterMemModule) extends PeekPokeTester(m) {
@@ -23,15 +31,28 @@ class MemPokeTester(m: OuterMemModule) extends PeekPokeTester(m) {
     pokeAt(m.billy, i, i)
     pokeAt(m.inner.nelly, value = i + 1, off = i)
   }
+
   step(10)
+
+  // This uses direct access reading
   0 until 1024 foreach { i =>
     expect(peekAt(m.billy, i) == BigInt(i), s"expected $i at $i, but found ${peekAt(m.billy, i)}")
     expect(peekAt(m.inner.nelly, i) == BigInt(i + 1), s"expected $i at $i, but found ${peekAt(m.billy, i)}")
   }
+
+  // This shows that the ordinary memory systems sees the values written with pokeAt
+  0 until 1024 foreach { i =>
+    poke(m.io.readAddress, i)
+    step(1)
+
+    expect(peek(m.io.readData) == BigInt(i), s"expected $i at $i, but found ${peek(m.io.readData)}")
+  }
+
+
 }
 
 class MemPokeSpec extends ChiselFlatSpec with Checkers {
-  behavior of "TestMem"
+  behavior of "Peeking and Poking straight into underlying memory, in interpreter"
 
   it should "return peek values exactly as poked" in
     check(Driver.execute(Array(), () => new OuterMemModule) { m => new MemPokeTester(m) })
