8acd0eacf921bf42e414221c04246c9c9e85f2d9
Jim Lawson <ucbjrl@berkeley.edu>
2018-05-14 11:00:20 -0700

    More BlackBoxSourceHelper cleanup adaptation

--- src/main/scala/chisel3/iotesters/PeekPokeTesterUtils.scala
+++ src/main/scala/chisel3/iotesters/PeekPokeTesterUtils.scala
@@ -93,7 +93,7 @@ private[iotesters] object verilogToIVL {
       moreIvlFlags: Seq[String] = Seq.empty[String]): Seq[String] = {
 
     val blackBoxVerilogList = {
-      val list_file = new File(dir, firrtl.transforms.BlackBoxSourceHelper.FileListName)
+      val list_file = new File(dir, firrtl.transforms.BlackBoxSourceHelper.fileListName)
       if(list_file.exists()) {
         Seq("-f", list_file.getAbsolutePath)
       }
