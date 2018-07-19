1112196614af89b265c3717e4ee22a4fdef09a47
chick <chick@qrhino.com>
2018-07-18 23:02:24 -0700

    LoadMemory test examples - Added memory test resource files - Tests now use resource files - Complex test does not use suffixes on mem files - Other test does use suffixes - Both tests now actually test values in addition to printing them out

--- src/test/scala/examples/ComplexMemoryLoadingSpec.scala
+++ src/test/scala/examples/ComplexMemoryLoadingSpec.scala
@@ -2,9 +2,13 @@
 
 package examples
 
+import java.nio.file.{Files, Paths}
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
+
 import chisel3._
 import chisel3.iotesters.PeekPokeTester
 import chisel3.util.{ChiselLoadMemoryAnnotation, log2Ceil}
+import firrtl.FileUtils
 import org.scalatest.{FreeSpec, Matchers}
 
 class MemoryShape extends Bundle {
@@ -21,26 +25,43 @@ class HasComplexMemory(memoryDepth: Int) extends Module {
 
   val memory = Mem(memoryDepth, new MemoryShape)
 
-  chisel3.experimental.annotate(ChiselLoadMemoryAnnotation(memory, "test_run_dir/examples.LoadMemoryFromFileSpec1251342320/mem1.txt"))
+  chisel3.experimental.annotate(ChiselLoadMemoryAnnotation(memory, "test_run_dir/complex_mem_test/mem"))
   io.value := memory(io.address)
 }
 
 class HasComplexMemoryTester(c: HasComplexMemory) extends PeekPokeTester(c) {
+  var boolValue: Int = 0
   for(addr <- 0 until 8) {
     poke(c.io.address, addr)
     step(1)
     println(f"peek from $addr ${peek(c.io.value.a)}%x ${peek(c.io.value.b)}%x ${peek(c.io.value.c)}%x")
+    expect(c.io.value.a, addr)
+    expect(c.io.value.b, 7 - addr)
+    expect(c.io.value.c, boolValue)
+    boolValue = 1 - boolValue
   }
 }
 
 
 class ComplexMemoryLoadingSpec extends  FreeSpec with Matchers {
   "memory loading should be possible with complex memories" in {
-    val result = iotesters.Driver.execute(
-      args = Array("--backend-name", "verilator"),
+
+    val targetDirName = "test_run_dir/complex_mem_test"
+    FileUtils.makeDirectory(targetDirName)
+
+    val path1 = Paths.get(targetDirName + "/mem_a")
+    val path2 = Paths.get(targetDirName + "/mem_b")
+    val path3 = Paths.get(targetDirName + "/mem_c")
+
+    Files.copy(getClass.getResourceAsStream("/mem1.txt"), path1, REPLACE_EXISTING)
+    Files.copy(getClass.getResourceAsStream("/mem2.txt"), path2, REPLACE_EXISTING)
+    Files.copy(getClass.getResourceAsStream("/mem3.txt"), path3, REPLACE_EXISTING)
+
+    iotesters.Driver.execute(
+      args = Array("--backend-name", "verilator", "--target-dir", targetDirName, "--top-name", "complex_mem_test"),
       dut = () => new HasComplexMemory(memoryDepth = 8)
     ) { c =>
       new HasComplexMemoryTester(c)
-    }
+    } should be (true)
   }
 }
