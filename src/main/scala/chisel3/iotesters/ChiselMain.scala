5af99ee0a4effe3e8345abe7a058af6286bab9ad
Jim Lawson <ucbjrl@berkeley.edu>
2020-01-08 09:47:11 -0800

    Merge with master

--- src/main/scala/chisel3/iotesters/ChiselMain.scala
+++ src/main/scala/chisel3/iotesters/ChiselMain.scala
@@ -65,7 +65,7 @@ object chiselMain {
       case "vcs" | "glsim" =>
         val harness = new FileWriter(new File(dir, s"${chirrtl.main}-harness.v"))
         val waveform = new File(dir, s"${chirrtl.main}.vpd").toString
-        genVCSVerilogHarness(dut, harness, waveform.toString, context.backendType == "glsim")
+        genVCSVerilogHarness(dut, harness, waveform.toString, context.backendType == "glsim", false)
       case b => throw BackendException(b)
     }
   }
